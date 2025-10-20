e611c9f4213582f27905661c42cfa6dbf6d18770
takeboat <929433643@qq.com>
2025-10-20 22:01:03 +0800

    feat:client优化

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,30 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net"
+	"time"
 )
 
+// mock 模拟客户端
 func main() {
 	fmt.Println("client start...")
+	// 创建链接 获取conn
 	conn, err := net.Dial("tcp", "0.0.0.0:8999")
 	if err != nil {
 		fmt.Println("dial error:", err)
 		panic(err)
 	}
 	defer conn.Close()
-
 	defer func() {
 		fmt.Println("client exit...")
 	}()
 	fmt.Println("client link success...")
-
-	// 发送数据
-	conn.Write([]byte("Hello from client\n"))
-
-	// 读取响应
-	buf := make([]byte, 512)
-	n, err := conn.Read(buf)
-	if err == nil {
-		fmt.Printf("收到响应: %s\n", string(buf[:n]))
+	for {
+		// 发送数据
+		_, err := conn.Write([]byte("Hello from client\n"))
+		if err != nil {
+			slog.Error("write error:", "err", err)
+			return
+		}
+		// 读取响应
+		buf := make([]byte, 512)
+		n, err := conn.Read(buf)
+		if err != nil {
+			slog.Error("read error:", "err", err)
+			return
+		}
+		slog.Info("receive from server:", "data", string(buf[:n]))
+		// cpu 休眠1秒
+		time.Sleep(1 * time.Second)
 	}
 }
