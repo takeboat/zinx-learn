e4eda210d2edb22caaec2ec43764e9d4ad35799e
takeboat <929433643@qq.com>
2025-10-25 21:52:15 +0800

    feat(znet): 完善连接管理器实现并添加日志输出

--- znet/connmanager.go
+++ znet/connmanager.go
@@ -1,6 +1,8 @@
 package znet
 
 import (
+	"errors"
+	"fmt"
 	"sync"
 	"zinx/ziface"
 )
@@ -10,30 +12,47 @@ type ConnManager struct {
 	connLock    sync.RWMutex
 }
 
-func (c *ConnManager) Add(conn ziface.IConnection) {
-	c.connLock.Lock()
-	defer c.connLock.Unlock()
-	c.connections[conn.GetConnID()] = conn
-}
-
-func (c *ConnManager) Remove(conn ziface.IConnection) {
-	panic("TODO: Implement")
-}
-
-func (c *ConnManager) Get(connID uint32) (ziface.IConnection, error) {
-	panic("TODO: Implement")
-}
-
-func (c *ConnManager) Len() int {
-	panic("TODO: Implement")
-}
-
-func (c *ConnManager) ClearConn() {
-	panic("TODO: Implement")
-}
-
 func NewConnManager() *ConnManager {
 	return &ConnManager{
 		connections: make(map[uint32]ziface.IConnection),
 	}
+}
+
+func (c *ConnManager) Add(conn ziface.IConnection) {
+	c.connLock.Lock()
+	defer c.connLock.Unlock()
+	// 将conn连接添加到Connmanager
+	c.connections[conn.GetConnID()] = conn
+	fmt.Println("connection add to Connmanager successfully, conn num = ", c.Len())
+
+}
+
+func (c *ConnManager) Remove(conn ziface.IConnection) {
+	c.connLock.Lock()
+	defer c.connLock.Unlock()
+	delete(c.connections, conn.GetConnID())
+	fmt.Println("connection remove connid = ", conn.GetConnID(), "successfully. conn num = ", c.Len())
+}
+
+func (c *ConnManager) Get(connID uint32) (ziface.IConnection, error) {
+	c.connLock.RLock()
+	defer c.connLock.RUnlock()
+	if conn, ok := c.connections[connID]; ok {
+		return conn, nil
+	}
+	return nil, errors.New("connection not found")
+}
+
+func (c *ConnManager) Len() int {
+	return len(c.connections)
+}
+
+func (c *ConnManager) ClearConn() {
+	c.connLock.Lock()
+	defer c.connLock.Unlock()
+	for connID, conn := range c.connections {
+		conn.Stop()
+		delete(c.connections, connID)
+	}
+	fmt.Println("Clear All Connections Successfully: conn num = ", c.Len())
 }
